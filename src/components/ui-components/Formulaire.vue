3c917bbb47ea564bf40b51f963b452eea30f7038
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2021-04-08 21:16:56 +0200

    Création du composant Formulaire pour une validation groupée

---
+++ src/components/ui-components/Formulaire.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <slot></slot>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { BusEvénements } from "@/BusEvénements";
+
+export default Vue.extend({
+  methods: {
+    validerFormulaire(): boolean {
+      var valide = true;
+      // Test de la validité de chaque composant enfant faisant objet à une validation (tous ceux qui possèdent
+      // la variable 'élémentFormulaire').
+      this.$children.forEach((child) => {
+        // Ce commentaire permet d'ignorer l'erreur de TypeScript à cause des
+        // propriétés inconnues 'estValide' et 'champRequisVide' sur les composants enfants.
+        // @ts-ignore
+        if (child.élémentFormulaire && !(child.estValide && !child.champRequisVide)) valide = false;
+      });
+      this.$emit("input", valide);
+      return valide;
+    },
+  },
+
+  mounted() {
+    this.validerFormulaire();
+    BusEvénements.$on("changement-état-validité", this.validerFormulaire);
+  },
+});
+</script>
