06884e5d6288b37b33937ab596debc0de626a9d6
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2021-04-24 16:05:09 +0200

    💄 (Toute l'application): Créer de nouvelles routes et améliorer la sécurité
    Ce commit permet de créer les routes de l'application et implémenter la protection de certaines routes pour les utilisateurs non authentifiés grâce aux 'route gards'.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import { auth } from "@/firebase";
 import Accueil from "@/views/Accueil.vue";
 import Encyclopedie from "@/views/Encyclopedie.vue";
 import DetailsCompositeur from "@/views/DetailsCompositeur.vue";
 import ModifierInfosCompositeur from "@/views/ModifierInfosCompositeur.vue";
 import NonTrouve from "@/views/NonTrouve.vue";
+import MaPartotheque from "@/views/MaPartotheque.vue";
 
 Vue.use(VueRouter);
 
@@ -13,6 +15,14 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Accueil",
     component: Accueil,
+  },
+  {
+    path: "/ma-partotheque",
+    name: "MaPartothèque",
+    component: MaPartotheque,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/encyclopedie",
@@ -30,11 +40,17 @@ const routes: Array<RouteConfig> = [
     name: "ModifierInfosCompositeur",
     component: ModifierInfosCompositeur,
     props: true,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/nouveau-compositeur",
     name: "NouveauCompositeur",
     component: ModifierInfosCompositeur,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/:catchAll(.*)",
@@ -47,6 +63,17 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = auth.currentUser;
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  if (requiresAuth && !currentUser) {
+    // next({ name: "Accueil", query: { suivant: to.name } });
+    next({ name: "Accueil" });
+  } else {
+    next();
+  }
 });
 
 export default router;
