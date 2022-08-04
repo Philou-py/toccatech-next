f6676deffd143111886f50214338dbfdb6d0af44
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2022-08-04 22:54:22 +0200

    🔍️ (_document.tsx): Add lang attribute to HTML tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="fr">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;700&display=swap"
