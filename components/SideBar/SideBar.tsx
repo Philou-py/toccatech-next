dcc1220fe6649b15a0a1f2e5af2488b69f37b609
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2022-08-05 11:56:47 +0200

    💄 (SideBar): Simplify sidebar transition and always keep it mounted

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -2,7 +2,6 @@ import { useCallback, useContext, useRef, MouseEvent, memo } from "react";
 import Link from "next/link";
 import sideBarStyles from "./SideBar.module.scss";
 import cn from "classnames";
-import { CSSTransition } from "react-transition-group";
 import { AuthContext } from "../../contexts/AuthContext";
 import { Button, Avatar } from "..";
 
@@ -40,83 +39,58 @@ function SideBar({ showSideBar, navLinks, handleAuth, onClose }: SideBarProps) {
 
   return (
     <div className={sideBarStyles.sidebar}>
-      <CSSTransition
-        nodeRef={bgRef}
-        in={showSideBar}
-        mountOnEnter
-        unmountOnExit
-        timeout={300}
-        classNames={{
-          enter: sideBarStyles.tBgEnter,
-          enterActive: sideBarStyles.tBgEnterActive,
-          exit: sideBarStyles.tBgExit,
-          exitActive: sideBarStyles.tBgExitActive,
-        }}
+      <div
+        className={cn(sideBarStyles.bg, { [sideBarStyles.show]: showSideBar })}
+        ref={bgRef}
+        onClick={handleSideBarClose}
       >
-        <div className={sideBarStyles.bg} ref={bgRef} onClick={handleSideBarClose}>
-          <CSSTransition
-            nodeRef={refForTransition2}
-            in={showSideBar}
-            appear
-            timeout={300}
-            classNames={{
-              appear: sideBarStyles.tSidebarAppear,
-              appearActive: sideBarStyles.tSidebarAppearActive,
-              enter: sideBarStyles.tSidebarEnter,
-              enterActive: sideBarStyles.tSidebarEnterActive,
-              exit: sideBarStyles.tSidebarExit,
-              exitActive: sideBarStyles.tSidebarExitActive,
-            }}
-          >
-            <div className={sideBarStyles.content} ref={refForTransition2}>
-              {!isAuthenticated && (
-                <Link href="/">
-                  <a style={{ textDecoration: "none" }}>
-                    <h3 className={sideBarStyles.title}>Toccatech</h3>
-                  </a>
-                </Link>
-              )}
-              {isAuthenticated && currentUser!.avatarURL && (
-                <Avatar
-                  type="image-avatar"
-                  className={sideBarStyles.avatar}
-                  borderColour="#33c9ff"
-                  src={currentUser!.avatarURL}
-                  size={150}
-                />
-              )}
-              {isAuthenticated && !currentUser!.avatarURL && (
-                <Avatar
-                  type="initials-avatar"
-                  className={sideBarStyles.avatar}
-                  initials={currentUser!.username
-                    .split(" ")
-                    .map((part) => part[0].toUpperCase())
-                    .join("")}
-                  borderColour="#33c9ff"
-                  size={150}
-                />
-              )}
+        <div
+          className={cn(sideBarStyles.content, { [sideBarStyles.show]: showSideBar })}
+          ref={refForTransition2}
+        >
+          {!isAuthenticated && (
+            <Link href="/">
+              <a style={{ textDecoration: "none" }}>
+                <h3 className={sideBarStyles.title}>Toccatech</h3>
+              </a>
+            </Link>
+          )}
+          {isAuthenticated && currentUser!.avatarURL && (
+            <Avatar
+              type="image-avatar"
+              className={sideBarStyles.avatar}
+              borderColour="#33c9ff"
+              src={currentUser!.avatarURL}
+              size={150}
+            />
+          )}
+          {isAuthenticated && !currentUser!.avatarURL && (
+            <Avatar
+              type="initials-avatar"
+              className={sideBarStyles.avatar}
+              initials={currentUser!.username
+                .split(" ")
+                .map((part) => part[0].toUpperCase())
+                .join("")}
+              borderColour="#33c9ff"
+              size={150}
+            />
+          )}
 
-              <nav className={sideBarStyles.navList}>
-                {navLinks.map(([name, url]) => (
-                  <Link href={url} key={name}>
-                    <a>{name}</a>
-                  </Link>
-                ))}
-              </nav>
-              {handleAuth && (
-                <Button
-                  className={cn("blue-grey", sideBarStyles.authButton)}
-                  onClick={signInSignOut}
-                >
-                  {isAuthenticated ? "Déconnexion" : "Connexion"}
-                </Button>
-              )}
-            </div>
-          </CSSTransition>
+          <nav className={sideBarStyles.navList}>
+            {navLinks.map(([name, url]) => (
+              <Link href={url} key={name}>
+                <a>{name}</a>
+              </Link>
+            ))}
+          </nav>
+          {handleAuth && (
+            <Button className={cn("blue-grey", sideBarStyles.authButton)} onClick={signInSignOut}>
+              {isAuthenticated ? "Déconnexion" : "Connexion"}
+            </Button>
+          )}
         </div>
-      </CSSTransition>
+      </div>
     </div>
   );
 }
